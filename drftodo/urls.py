b3b428f92f458140be3e68449ae32d97fd7759a2
Md.Taohidul Islam <mdtaohidul73@student.sust.edu>
2024-11-13 16:54:34 +0600

    added Swagger API documentation and updated requirements

--- drftodo/urls.py
+++ drftodo/urls.py
@@ -19,9 +19,28 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from app.views import index
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API documentation",
+        default_version='v1',
+        description="API documentation for drftodo",
+        terms_of_service="https://www.example.com/terms/",
+        contact=openapi.Contact(email="contact@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", index,name="index"),
     path("api/", include("app.api.urls")),
+    ## Swagger
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
